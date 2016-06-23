0dbee69d713984985ead6c174786f34ace27e9d8
仙枫 <shiwen.zsw@alibaba-inc.com>
2016-06-23 16:46:57 +0800

    add

--- assets/js/loaded.js
+++
@@ -1,3 +0,0 @@
-var imgArr = ['http://img03.taobaocdn.com/imgextra/i3/187929522/TB2_ibGcXXXXXaNXpXXXXXXXXXX_!!187929522.jpg', 'http://img01.taobaocdn.com/imgextra/i1/187929522/TB2wSbhcXXXXXbJXXXXXXXXXXXX_!!187929522.jpg'];
-var random = parseInt(Math.random() * 2);
-document.getElementsByClassName('bg')[0].src = imgArr[random];
