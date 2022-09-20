a0c6adb7a00a2ef737c8a5a0eb5aa21190e2dedb
Shiwen Zhu <zhusw@zhejianglab.com>
2022-09-20 10:43:29 +0800

    deploy

--- deploy.js
+++ deploy.js
@@ -1,21 +1,3 @@
-// # 确保脚本抛出遇到的错误
-// set -e
-
-// # 打包生成静态文件
-// npm run build
-
-// # 进入打包好的文件夹
-// cd docs/.vuepress/dist
-
-// # 创建git的本地仓库，提交修改
-// git init
-// git add -A
-// git commit -m 'deploy'
-
-// # 覆盖式地将本地仓库发布至github，因为发布不需要保留历史记录
-// # 格式为：git push -f git@github.com:'用户名'/'仓库名'.git master
-// git push -f git@github.com:shelwinjue/shelwinjue.github.io.git master
-
 const { execSync } = require('child_process');
 
 execSync('npm run build', (err, stdout, stderr) => {
@@ -53,9 +35,12 @@ execSync('git commit -m "deploy"', (err, stdout, stderr) => {
   }
 });
 
-execSync('git push -f origin master', (err, stdout, stderr) => {
-  if (err) {
-    console.log(err);
-    return;
+execSync(
+  'git push -f git@github.com:shelwinjue/shelwinjue.github.io.git master',
+  (err, stdout, stderr) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
   }
-});
+);
--- docs/README.md
+++ docs/README.md
@@ -5,7 +5,7 @@ tagline: 站点由Vuepress生成
 actionText: 起步
 actionLink: /pages/components/HelloWorld
 features:
-  - title: 简洁至上
+  - title: 文档规范
     details: 以 Markdown 为中心的项目结构，以最少的配置帮助你专注于写作。
   - title: Vue驱动
     details: 享受 Vue + webpack 的开发体验，在 Markdown 中使用 Vue 组件，同时可以使用 Vue 来开发自定义主题。
