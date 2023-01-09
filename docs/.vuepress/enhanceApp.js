0b852778bba83066a775e36b7aa8b3f42b00c2d8
Shiwen Zhu <zhusw@zhejianglab.com>
2023-01-09 15:15:19 +0800

    feat: 🎸 修改codemirror打包问题

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -2,11 +2,15 @@ import Vue from 'vue';
 import '@zjlabvis/lowcode-index/dist/jianweiComponents.css';
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
-import VueCodemirror from 'vue-codemirror';
-import 'codemirror/mode/vue/vue.js';
+let VueCodemirror = null;
+// import VueCodemirror from 'vue-codemirror';
+// import 'codemirror/mode/vue/vue.js';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
 import './global.css';
-
+if (typeof window !== 'undefined' && typeof window.navigator !== 'undefined') {
+  VueCodemirror = require('vue-codemirror');
+  require('codemirror/mode/vue/vue.js');
+  Vue.use(VueCodemirror);
+}
 Vue.use(Antd);
-Vue.use(VueCodemirror);
