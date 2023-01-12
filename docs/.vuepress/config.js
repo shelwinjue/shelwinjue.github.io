4b9538ebb48444b5998dc7a37115f62fa999875a
Shiwen Zhu <zhusw@zhejianglab.com>
2023-01-12 16:44:31 +0800

    feat: 🎸 去掉站点无效的几个导航

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,16 +10,16 @@ module.exports = {
       { text: '首页', link: '/' },
 
       //格式二：添加下拉菜单，link指向的文件路径
-      {
-        text: '分类', //默认显示
-        ariaLabel: '分类', //用于识别的label
-        items: [
-          { text: '文章', link: '/pages/folder1/test1.md' },
-          //点击标签会跳转至link的markdown文件生成的页面
-          { text: '琐碎', link: '/pages/folder2/test4.md' },
-        ],
-      },
-      { text: '功能演示', link: '/pages/folder1/test3.md' },
+      // {
+      //   text: '分类', //默认显示
+      //   ariaLabel: '分类', //用于识别的label
+      //   items: [
+      //     { text: '文章', link: '/pages/folder1/test1.md' },
+      //     //点击标签会跳转至link的markdown文件生成的页面
+      //     { text: '琐碎', link: '/pages/folder2/test4.md' },
+      //   ],
+      // },
+      // { text: '功能演示', link: '/pages/folder1/test3.md' },
 
       //格式三：跳转至外部网页，需http/https前缀
       {
